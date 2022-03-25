0411280737c0f5dd00ba2cd9ceecfd7a131592d0
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-03-25 19:57:11 +0100

    Added interface classes
    In order to give the opportunity to my colleagues to work with my classes merging my branch

---
+++ src/main/java/it/polimi/ingsw/model/board/Bag.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.model.board;
+
+import it.polimi.ingsw.model.utilities.HouseColor;
+
+import java.util.List;
+
+/**
+ * Bag Class
+ *
+ * @author Matteo Negro
+ */
+
+public class Bag {
+    private List<HouseColor> listStudents;
+
+    public Bag(List<HouseColor> listStudents) {
+    }
+
+    public HouseColor pop() {
+        return null;
+    }
+
+    public void push() {
+    }
+
+    private void randomize() {
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/model/board/Island.java
@@ -0,0 +1,64 @@
+package it.polimi.ingsw.model.board;
+
+import it.polimi.ingsw.model.utilities.HouseColor;
+import it.polimi.ingsw.model.utilities.TowerType;
+
+import java.util.Map;
+
+/**
+ * Island Class, board main element
+ *
+ * @author Matteo Negro
+ */
+
+public class Island {
+    private int id;
+    private int size;
+    private boolean ban;
+    private TowerType tower;
+    private Map<HouseColor, Integer> students;
+
+    public Island() {
+    }
+
+    public int getId() {
+        return 0;
+    }
+
+    public TowerType getTower() {
+        return null;
+    }
+
+    public int getSize() {
+        return 0;
+    }
+
+    public boolean isBan() {
+        return false;
+    }
+
+    public Map<HouseColor, Integer> getStudents() {
+        return null;
+    }
+
+    public Map<Player, Integer> getInfluence() {
+        return null;
+    }
+
+    public Map<Player, Integer> getInfluence(HouseColor banColor) {
+        return null;
+    }
+
+    private int getInfluence(Player player, HouseColor targetColor) {
+        return 0;
+    }
+
+    public void addStudent(HouseColor studentColor) {
+    }
+
+    public void setTower(TowerType tower) {
+    }
+
+    public void setBan() {
+    }
+}
