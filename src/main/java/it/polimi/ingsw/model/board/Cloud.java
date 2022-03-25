682ae4725bbc924eb5335d42c8019dd698406071
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-03-25 19:57:11 +0100

    Added interface classes
    In order to give the opportunity to my colleagues to work with my classes merging my branch

---
+++ src/main/java/it/polimi/ingsw/model/board/Cloud.java
@@ -0,0 +1,39 @@
+package it.polimi.ingsw.model.board;
+
+import it.polimi.ingsw.model.utilities.HouseColor;
+
+import java.util.Map;
+
+/**
+ * Cloud Class
+ *
+ * @author Matteo Negro
+ */
+
+public class Cloud {
+    private int id;
+    private int studentsNum;
+    private Map<HouseColor, Integer> students;
+
+    public Cloud() {
+    }
+
+    public int getId() {
+        return 0;
+    }
+
+    public int getStudentsNum() {
+        return 0;
+    }
+
+    public boolean isFull() {
+        return false;
+    }
+
+    public void refill() {
+    }
+
+    public Map<HouseColor, Integer> flush() {
+        return null;
+    }
+}
