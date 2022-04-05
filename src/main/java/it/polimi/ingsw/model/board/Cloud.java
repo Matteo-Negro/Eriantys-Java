29a474ffcc37ae57f42e9eb6cf41f308c59a5ee1
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-04-05 12:55:54 +0200

    Added Bag & Cloud class implementation and also a folder for exceptions

--- src/main/java/it/polimi/ingsw/model/board/Cloud.java
+++ src/main/java/it/polimi/ingsw/model/board/Cloud.java
@@ -2,38 +2,89 @@ package it.polimi.ingsw.model.board;
 
 import it.polimi.ingsw.model.utilities.HouseColor;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Cloud Class
+ * Cloud Class, it's one of GameBoard elements, here you can find the refill for the SchoolBoard's entrance
  *
  * @author Matteo Negro
  */
 
 public class Cloud {
-    private int id;
-    private int studentsNum;
-    private Map<HouseColor, Integer> students;
+    private final int id;
+    private final Map<HouseColor, Integer> students;
 
+    /**
+     * Cloud Constructor, listStudents is initialized with the all 130 students and shuffled
+     *
+     * @param idCloud it's a number in the interval [0;2] or [0;3], it's used to identified the cloud
+     */
     public Cloud(int idCloud) {
+        this.id = idCloud;
+        this.students = new HashMap<>();
+        Arrays.stream(HouseColor.values()).forEach(color -> {
+            this.students.put(color, 0);
+        });
     }
 
+    /**
+     * This method gives as result the id's island
+     *
+     * @return The id's island
+     */
     public int getId() {
-        return 0;
+        return this.id;
     }
 
-    public int getStudentsNum() {
-        return 0;
-    }
-
+    /**
+     * The method returns whether the cloud is full
+     *
+     * @return True if it's full
+     */
     public boolean isFull() {
+        if (this.students.get(HouseColor.BLUE) != 0) return true;
+        if (this.students.get(HouseColor.FUCHSIA) != 0) return true;
+        if (this.students.get(HouseColor.GREEN) != 0) return true;
+        if (this.students.get(HouseColor.YELLOW) != 0) return true;
+        if (this.students.get(HouseColor.RED) != 0) return true;
         return false;
     }
 
-    public void refill() {
+    /**
+     * The method refills the data structure where students are saved
+     *
+     * @param newStudents The students to have to put on the cloud
+     */
+    public void refill(Map<HouseColor, Integer> newStudents) {
+        Arrays.stream(HouseColor.values()).forEach(color -> {
+            this.students.put(color, newStudents.get(color));
+        });
     }
 
+    /**
+     * The method cleans the students on the cloud
+     *
+     * @return The data structure of the students when the method is called
+     */
     public Map<HouseColor, Integer> flush() {
-        return null;
+        Map<HouseColor, Integer> returnMap;
+
+        returnMap = this.getStudents();
+        Arrays.stream(HouseColor.values()).forEach(color -> {
+            this.students.put(color, 0);
+        });
+
+        return returnMap;
+    }
+
+    /**
+     * The method gives as result the numbers of students
+     *
+     * @return The data structure of the students when the method is called
+     */
+    public Map<HouseColor, Integer> getStudents() {
+        return new HashMap<>(this.students);
     }
 }
