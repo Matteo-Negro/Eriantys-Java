b81b80e5e73acd4dd28bd295379030f36ef05f1e
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-04-06 17:56:40 +0200

    Fixed Cloud class implementation & documentation

--- src/main/java/it/polimi/ingsw/model/board/Cloud.java
+++ src/main/java/it/polimi/ingsw/model/board/Cloud.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Cloud Class, it's one of GameBoard elements, here you can find the refill for the SchoolBoard's entrance
+ * Cloud Class, it's one of GameBoard elements, here you can find the refill for the SchoolBoard's entrance.
  *
  * @author Matteo Negro
  */
@@ -17,74 +17,63 @@ public class Cloud {
     private final Map<HouseColor, Integer> students;
 
     /**
-     * Cloud Constructor, listStudents is initialized with the all 130 students and shuffled
+     * Cloud Constructor, listStudents is initialized with the all 130 students and shuffled.
      *
-     * @param idCloud it's a number in the interval [0;2] or [0;3], it's used to identified the cloud
+     * @param idCloud it's a number in the interval [0;2] or [0;3], it's used to identified the cloud.
      */
     public Cloud(int idCloud) {
         this.id = idCloud;
         this.students = new HashMap<>();
-        Arrays.stream(HouseColor.values()).forEach(color -> {
-            this.students.put(color, 0);
-        });
+        Arrays.stream(HouseColor.values()).forEach(color -> this.students.put(color, 0));
     }
 
     /**
-     * This method gives as result the id's island
+     * This method gives as result the id's island.
      *
-     * @return The id's island
+     * @return The id's island.
      */
     public int getId() {
         return this.id;
     }
 
     /**
-     * The method returns whether the cloud is full
+     * The method gives as result the numbers of students.
      *
-     * @return True if it's full
+     * @return The data structure of the students when the method is called.
+     */
+    public Map<HouseColor, Integer> getStudents() {
+        return new HashMap<>(this.students);
+    }
+
+    /**
+     * The method returns whether the cloud is full.
+     *
+     * @return True if it's full.
      */
     public boolean isFull() {
-        if (this.students.get(HouseColor.BLUE) != 0) return true;
-        if (this.students.get(HouseColor.FUCHSIA) != 0) return true;
-        if (this.students.get(HouseColor.GREEN) != 0) return true;
-        if (this.students.get(HouseColor.YELLOW) != 0) return true;
-        if (this.students.get(HouseColor.RED) != 0) return true;
-        return false;
+        return Arrays.stream(HouseColor.values()).anyMatch(color -> this.students.get(color) != 0);
     }
 
     /**
-     * The method refills the data structure where students are saved
+     * The method refills the data structure where students are saved.
      *
-     * @param newStudents The students to have to put on the cloud
+     * @param newStudents The students to have to put on the cloud.
      */
     public void refill(Map<HouseColor, Integer> newStudents) {
-        Arrays.stream(HouseColor.values()).forEach(color -> {
-            this.students.put(color, newStudents.get(color));
-        });
+        Arrays.stream(HouseColor.values()).forEach(color -> this.students.put(color, newStudents.get(color)));
     }
 
     /**
-     * The method cleans the students on the cloud
+     * The method cleans the students on the cloud.
      *
-     * @return The data structure of the students when the method is called
+     * @return The data structure of the students when the method is called.
      */
     public Map<HouseColor, Integer> flush() {
         Map<HouseColor, Integer> returnMap;
 
         returnMap = this.getStudents();
-        Arrays.stream(HouseColor.values()).forEach(color -> {
-            this.students.put(color, 0);
-        });
+        Arrays.stream(HouseColor.values()).forEach(color -> this.students.put(color, 0));
 
         return returnMap;
-    }
-
-    /**
-     * The method gives as result the numbers of students
-     *
-     * @return The data structure of the students when the method is called
-     */
-    public Map<HouseColor, Integer> getStudents() {
-        return new HashMap<>(this.students);
     }
 }
