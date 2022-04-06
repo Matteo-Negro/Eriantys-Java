dc4eee71ef3cddc3b759e5595400bf00cc55d585
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-04-06 17:15:20 +0200

    Fixed Bag class implementation & documentation

--- src/main/java/it/polimi/ingsw/model/board/Bag.java
+++ src/main/java/it/polimi/ingsw/model/board/Bag.java
@@ -5,7 +5,7 @@ import it.polimi.ingsw.model.utilities.HouseColor;
 import java.util.*;
 
 /**
- * Bag Class, it's one of GameBoard elements, contains all the students before the deployment
+ * Bag Class, it's one of GameBoard elements, contains all the students before the deployment.
  *
  * @author Matteo Negro
  */
@@ -14,7 +14,7 @@ public class Bag {
     private Stack<HouseColor> listStudents;
 
     /**
-     * Bag Constructor, listStudents is initialized with the all 130 students and shuffled
+     * Bag Constructor, listStudents is initialized with the all 120 students and shuffled.
      */
     public Bag() {
         this.listStudents = new Stack<>();
@@ -29,19 +29,19 @@ public class Bag {
     }
 
     /**
-     * This method gets and removes the first element in the List
+     * This method gets and removes the first element in the List.
      *
-     * @return First element in the List
-     * @throws EmptyStackException The listStudents is empty
+     * @return First element in the List.
+     * @throws EmptyStackException The listStudents is empty.
      */
     public HouseColor pop() throws EmptyStackException {
         return this.listStudents.pop();
     }
 
     /**
-     * This method adds a student in the List and shuffles it
+     * This method adds a student in the List and shuffles it.
      *
-     * @param student The student (HouseColor) that the user would like to put back in the bag
+     * @param student The student (HouseColor) that the user would like to put back in the bag.
      */
     public void push(HouseColor student) {
         this.listStudents.push(student);
@@ -50,16 +50,9 @@ public class Bag {
     }
 
     /**
-     * This method shuffles the students in the List
-     */
-    public void randomize() {
-        Collections.shuffle(this.listStudents);
-    }
-
-    /**
-     * This method gives as result an arraylist to fill the GambeBoard the first time
+     * This method gives as result an arraylist to fill the GambeBoard the first time.
      *
-     * @return The arraylist with the sequence of students for the setup
+     * @return The arraylist with the sequence of students for the setup.
      */
     public ArrayList<HouseColor> boardSetUp() {
         ArrayList<HouseColor> setUp = new ArrayList<>();
@@ -70,5 +63,12 @@ public class Bag {
 
         Collections.shuffle(setUp);
         return setUp;
+    }
+
+    /**
+     * This method shuffles the students in the List.
+     */
+    private void randomize() {
+        Collections.shuffle(this.listStudents);
     }
 }
