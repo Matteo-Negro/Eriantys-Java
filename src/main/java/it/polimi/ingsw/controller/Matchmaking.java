a9db31ce62e96ea7889fc14d820673c02b3ebb5b
RiccardoMilici <miliciriccardo@tiscali.it>
2022-04-20 17:13:51 +0200

    Implemented Matchmaking methods, fixed User class

--- src/main/java/it/polimi/ingsw/controller/Matchmaking.java
+++ src/main/java/it/polimi/ingsw/controller/Matchmaking.java
@@ -1,16 +1,26 @@
 package it.polimi.ingsw.controller;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import it.polimi.ingsw.ServerLauncher;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Matchmaking extends Thread{
 
     private User user;
-    private ServerLauncher gameServer;
+    private final ServerLauncher gameServer;
 
+
+    /**
+     *
+     * @param userSocket
+     * @param gameServer
+     */
     public Matchmaking(Socket userSocket, ServerLauncher gameServer){
 
         this.user = null;
@@ -28,14 +38,23 @@ public class Matchmaking extends Thread{
         }
     }
 
+
+    /**
+     *
+     */
     public void run() {
         manageCommand();
     }
 
+
+    /**
+     *
+     */
     private void manageCommand() {
+
         JsonObject command = null;
 
-        do{
+        while(true) {
             try {
                 command = user.getCommand();
             }catch(IOException ioe) {
@@ -57,26 +76,143 @@ public class Matchmaking extends Thread{
                 return;
             }
 
-        }while(command == null);
+            // COMMAND PARSING
+            JsonElement type = command.get("type");
 
-        //command parsing
+            if(type.toString().equals("gameCreation")) {
+
+                //Create the game
+                String gameCode = createGame(command.get("playersNumber").getAsInt(), command.get("expert").getAsBoolean());
+
+                //Send a game creation reply
+                JsonObject gameCreationReply = new JsonObject();
+                gameCreationReply.addProperty("type", "gameCreation");
+                gameCreationReply.addProperty("code", gameCode);
+                try{
+                    user.sendCommand(gameCreationReply);
+                }catch(IOException ioe){
+                    //exception handle
+                }
+
+                //Join the game
+                searchGame(gameCode);
+            }
+            else if(type.toString().equals("enterGame")) {
+
+                searchGame(command.get("code").toString());
+            }
+        }
     }
 
-//    private void createGame(playersNumber, expertMode) {
-//        gameServer.addGame(playersNumber, expertMode);
-//    }
 
-//    private void searchGame(String gameId) {
-//
-//        GameController desiredGame = null;
-//
-//        try{
-//            desiredGame = gameServer.findGame(gameId);
-//            }catch(GameNotFoundException gnfe){
-//                 //send an error message to the client
-//                 return;
-//            }
-//
-//         gameServer.joinGame(user, desiredGame);
-//    }
+    /**
+     *
+     * @param playersNumber
+     * @param expertMode
+     * @return
+     */
+    private String createGame(int playersNumber, boolean expertMode) {
+
+        return gameServer.addGame(playersNumber, expertMode);
+    }
+
+
+    /**
+     *
+     * @param gameId
+     */
+    private void searchGame(String gameId) {
+
+        GameController desiredGame = null;
+        JsonObject playerEntranceMessage = null;
+        boolean loggedIn = false;
+        boolean isPresent = false;
+
+        try {
+            desiredGame = gameServer.findGame(gameId);
+        }catch(GameNotFoundException gnfe) {
+
+            //Send a game NOT found reply
+            JsonObject gameNotFoundReply = new JsonObject();
+            gameNotFoundReply.addProperty("type", "enterGame");
+            gameNotFoundReply.addProperty("found", true);
+            try {
+                user.sendCommand(gameNotFoundReply);
+            }catch(IOException ioe) {
+                //exception handle
+            }
+            return;
+        }
+
+        //Send a game found reply
+        JsonObject gameFoundReply = new JsonObject();
+        gameFoundReply.addProperty("type", "enterGame");
+        gameFoundReply.addProperty("found", true);
+        gameFoundReply.addProperty("expectedPlayers", desiredGame.getExpectedPlayers);
+        JsonArray playersList = new JsonArray();
+        for(User savedPlayer : desiredGame.getUsers()){
+
+            JsonObject player = new JsonObject();
+            player.addProperty("name", savedPlayer.getName());
+            player.addProperty("online", savedPlayer.getIsConnected());
+            playersList.add(player);
+        }
+
+        try {
+            user.sendCommand(gameFoundReply);
+        }catch(IOException ioe) {
+            //exception handle
+        }
+
+
+        // LOGGING IN
+        while(!loggedIn) {
+            do {
+                try {
+                    playerEntranceMessage = user.getCommand();
+                } catch (IOException ioe) {
+                    //exception handle
+                } catch (ClassNotFoundException cnfe) {
+                    //exception handle
+                }
+            } while (!playerEntranceMessage.get("type").toString().equals("login") && !playerEntranceMessage.get("type").toString().equals("exit"));
+
+            // A LOGIN MESSAGE ARRIVED
+            if (playerEntranceMessage.get("type").toString().equals("login")) {
+
+                user.putName(playerEntranceMessage.get("name").getAsString());
+
+                for (User savedPlayer : desiredGame.getUsers()) {
+
+                    if (user.getName().equals(savedPlayer.getName()) && !savedPlayer.getIsConnected()) {
+
+                        isPresent = true;
+                        desiredGame.getUsers().remove(savedPlayer);
+                        loggedIn = true;
+                    } else {
+
+                        if (user.getName().equals(savedPlayer.getName()) && savedPlayer.getIsConnected()) {
+
+                            isPresent = true;
+                            //send a user already present message
+                            break;
+                        }
+                    }
+                }
+                if(!isPresent && desiredGame.getUsers().size()<desiredGame.getExpectedPlayers()) {
+
+                    loggedIn = true;
+                }
+
+            }
+            else {
+                //AN EXIT MESSAGE ARRIVED
+                if(playerEntranceMessage.get("type").toString().equals("exit")) {
+                    return;
+                }
+            }
+        }
+
+        gameServer.joinGame(user, desiredGame);
+    }
 }
--- src/main/java/it/polimi/ingsw/controller/User.java
+++ src/main/java/it/polimi/ingsw/controller/User.java
@@ -1,24 +1,30 @@
 package it.polimi.ingsw.controller;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class User {
 
     private int id;
-    private Socket socket;
+    private final Socket socket;
     private String name;
+    private boolean isConnected;
     private ObjectInputStream socketInputStream;
+    private ObjectOutputStream socketOutputStream;
 
     public User(Socket socket) throws IOException{
         this.id = -1;
         this.socket = socket;
         this.name = null;
+        this.isConnected = false;
 
         socketInputStream = new ObjectInputStream(socket.getInputStream());
+        socketOutputStream = new ObjectOutputStream(socket.getOutputStream());
     }
 
     public void putName(String username) {
@@ -29,10 +35,8 @@ public class User {
         this.id = userId;
     }
 
-    public void putSocket(Socket userSocket) throws IOException{
-        this.socket = userSocket;
-
-        socketInputStream = new ObjectInputStream(socket.getInputStream());
+    public void setIsConnected(boolean connectionStatus) {
+        this.isConnected = connectionStatus;
     }
 
     public int getId() {
@@ -47,7 +51,19 @@ public class User {
         return name;
     }
 
+    public boolean getIsConnected() {
+        return isConnected;
+    }
+
     public JsonObject getCommand() throws IOException, ClassNotFoundException{
-        return (JsonObject) socketInputStream.readObject();
+
+        String message = (String) socketInputStream.readObject();
+        return JsonParser.parseString(message).getAsJsonObject();
+    }
+
+    public void sendCommand(JsonObject command) throws IOException{
+
+        String message = command.getAsString();
+        socketOutputStream.writeObject(message);
     }
 }
