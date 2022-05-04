a21a69b35c2e881a7262e1ce6a01d26588dcd983
mrmotta <61902921+mrmotta@users.noreply.github.com>
2022-05-04 19:21:47 +0200

    Added HerbalistEffectTest

---
+++ src/test/java/it/polimi/ingsw/model/board/effects/HerablistEffectTest.java
@@ -0,0 +1,68 @@
+package it.polimi.ingsw.model.board.effects;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test of class HerbalistEffect.
+ *
+ * @author Riccardo Motta
+ */
+class HerablistEffectTest {
+
+    HerablistEffect herablistEffect;
+
+    /**
+     * Generates a new HerbalistEffect.
+     */
+    @BeforeEach
+    void setUp() {
+        herablistEffect = new HerablistEffect();
+    }
+
+    /**
+     * Allows the garbage collector to delete the created FarmerEffect.
+     */
+    @AfterEach
+    void tearDown() {
+        herablistEffect = null;
+    }
+
+    /**
+     * Tests if returns the correct ID.
+     */
+    @Test
+    void getId() {
+        assertEquals(5, herablistEffect.getId());
+    }
+
+    /**
+     * Tests if the effect works correctly.
+     */
+    @Test
+    void effect() {
+        herablistEffect.effect("take");
+        assertEquals(4, herablistEffect.getAvailableBans());
+        herablistEffect.effect("restore");
+        assertEquals(5, herablistEffect.getAvailableBans());
+    }
+
+    /**
+     * Tests if returns the correct cost.
+     */
+    @Test
+    void getCost() {
+        assertEquals(2, herablistEffect.getCost());
+    }
+
+    /**
+     * Tests if the number of available beans is correct.
+     */
+    @Test
+    void getAvailableBans() {
+        assertEquals(5, herablistEffect.getAvailableBans());
+    }
+}
