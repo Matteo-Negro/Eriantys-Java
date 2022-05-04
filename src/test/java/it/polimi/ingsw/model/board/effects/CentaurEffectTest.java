6d01679fa9afb2036a93b80d8c8e191974506a9c
mrmotta <61902921+mrmotta@users.noreply.github.com>
2022-05-04 19:21:14 +0200

    Added CentaurEffectTest

---
+++ src/test/java/it/polimi/ingsw/model/board/effects/CentaurEffectTest.java
@@ -0,0 +1,49 @@
+package it.polimi.ingsw.model.board.effects;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test of class CentaurEffect.
+ *
+ * @author Riccardo Motta
+ */
+class CentaurEffectTest {
+
+    CentaurEffect centaurEffect;
+
+    /**
+     * Generates a new CentaurEffect.
+     */
+    @BeforeEach
+    void setUp() {
+        centaurEffect = new CentaurEffect();
+    }
+
+    /**
+     * Allows the garbage collector to delete the created FarmerEffect.
+     */
+    @AfterEach
+    void tearDown() {
+        centaurEffect = null;
+    }
+
+    /**
+     * Tests if returns the correct ID.
+     */
+    @Test
+    void getId() {
+        assertEquals(6, centaurEffect.getId());
+    }
+
+    /**
+     * Tests if returns the correct cost.
+     */
+    @Test
+    void getCost() {
+        assertEquals(3, centaurEffect.getCost());
+    }
+}
