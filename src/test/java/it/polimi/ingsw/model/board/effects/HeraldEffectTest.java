156e10c170c7e6a321c7214442d3a25a70cbba2e
mrmotta <61902921+mrmotta@users.noreply.github.com>
2022-05-04 19:23:30 +0200

    Added HeraldEffectTest

---
+++ src/test/java/it/polimi/ingsw/model/board/effects/HeraldEffectTest.java
@@ -0,0 +1,49 @@
+package it.polimi.ingsw.model.board.effects;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test of class HeraldEffect.
+ *
+ * @author Riccardo Motta
+ */
+class HeraldEffectTest {
+
+    HeraldEffect heraldEffect;
+
+    /**
+     * Generates a new HeraldEffect.
+     */
+    @BeforeEach
+    void setUp() {
+        heraldEffect = new HeraldEffect();
+    }
+
+    /**
+     * Allows the garbage collector to delete the created FarmerEffect.
+     */
+    @AfterEach
+    void tearDown() {
+        heraldEffect = null;
+    }
+
+    /**
+     * Tests if returns the correct ID.
+     */
+    @Test
+    void getId() {
+        assertEquals(3, heraldEffect.getId());
+    }
+
+    /**
+     * Tests if returns the correct cost.
+     */
+    @Test
+    void getCost() {
+        assertEquals(3, heraldEffect.getCost());
+    }
+}
