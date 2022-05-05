e7c7cbccafb4eff26e8328343c7ebba79a7c6d9f
mrmotta <61902921+mrmotta@users.noreply.github.com>
2022-05-05 10:25:05 +0200

    First basic draft of client

--- src/main/java/it/polimi/ingsw/network/client/Client.java
+++ src/main/java/it/polimi/ingsw/network/client/Client.java
@@ -1,4 +1,17 @@
 package it.polimi.ingsw.network.client;
 
 public class Client {
+
+    /**
+     * Graphics types: CLI or GUI.
+     */
+    public enum GraphicsType {CLI, GUI}
+
+    /**
+     * Default constructor.
+     *
+     * @param graphicsType The type of the interface that has to be instanced.
+     */
+    public Client(GraphicsType graphicsType) {
+    }
 }
