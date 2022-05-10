e8e4983b2be57ecb4d49f4aca780663e52c138ef
RiccardoMilici <miliciriccardo@tiscali.it>
2022-05-10 20:24:41 +0200

    Added raw client controller and status class structure

---
+++ src/main/java/it/polimi/ingsw/clientController/ClientController.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.clientController;
+
+import it.polimi.ingsw.clientStatus.Status;
+
+public class ClientController {
+    private String userName;
+    private GameServer gameServer;
+    private Status status;
+    private ClientState state;
+    private String phase;
+    private GameControllerStates subPhase;
+
+
+}
