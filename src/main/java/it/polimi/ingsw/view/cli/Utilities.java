0f5e95ef4ef6f02fa0cc52093de310d12507ca4d
mrmotta <61902921+mrmotta@users.noreply.github.com>
2022-05-12 10:27:25 +0200

    CLI: added error message printing.

--- src/main/java/it/polimi/ingsw/view/cli/Utilities.java
+++ src/main/java/it/polimi/ingsw/view/cli/Utilities.java
@@ -87,6 +87,30 @@ public class Utilities {
     }
 
     /**
+     * Prints an error message on the top of the screen.
+     *
+     * @param terminal Terminal where to execute.
+     * @param message  Message to print
+     */
+    public static void printError(Terminal terminal, String message) {
+        Ansi ansi = new Ansi();
+        ansi.bgRed();
+        foreground(ansi, White.getInstance());
+        for (int index = 0; index < terminal.getWidth(); index++)
+            ansi.append(" ");
+        bold(ansi, true);
+        ansi.cursor(0, (terminal.getWidth() - message.length() - 7) / 2);
+        ansi.append("Error");
+        bold(ansi, false);
+        ansi.append(": ");
+        ansi.append(message);
+        ansi.cursor(0, 0);
+        background(ansi, Black.getInstance());
+        terminal.writer().print(ansi);
+        terminal.flush();
+    }
+
+    /**
      * Gets the color of the student from HouseColor.
      *
      * @param houseColor The student's color.
