ac0fccc922eb34ed372d8748cd97844eecdae5e4
mrmotta <61902921+mrmotta@users.noreply.github.com>
2022-05-20 10:58:33 +0200

    GUI: added game creation page

--- src/main/java/it/polimi/ingsw/client/ClientGui.java
+++ src/main/java/it/polimi/ingsw/client/ClientGui.java
@@ -22,7 +22,7 @@ public class ClientGui extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root;
         try {
-            root = FXMLLoader.load(getClass().getResource("/fxml/Menu.fxml"));
+            root = FXMLLoader.load(getClass().getResource("/fxml/GameCreation.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
             return;
