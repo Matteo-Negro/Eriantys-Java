34007901c64527c46ef0bb357f7f796c1a5e05aa
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-05-24 14:37:03 +0200

    Added CommandParser interface to manage command client side

---
+++ src/main/java/it/polimi/ingsw/utilities/parsers/CommandParser.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.utilities.parsers;
+
+import com.google.gson.JsonObject;
+
+public class CommandParser {
+    private CommandParser() {
+    }
+
+    public static JsonObject commandManager(String command) {
+        JsonObject jsonCommand = null;
+        // TODO: parser
+        return jsonCommand;
+    }
+
+}
