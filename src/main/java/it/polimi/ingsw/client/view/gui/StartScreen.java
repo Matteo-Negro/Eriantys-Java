ecaa3bf810f59f39dee14623093b4241b376bacc
mrmotta <61902921+mrmotta@users.noreply.github.com>
2022-05-28 16:38:49 +0200

    CLI: few litte bug fixes

--- src/main/java/it/polimi/ingsw/client/view/gui/StartScreen.java
+++ src/main/java/it/polimi/ingsw/client/view/gui/StartScreen.java
@@ -11,7 +11,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Objects;
 
 public class StartScreen {
