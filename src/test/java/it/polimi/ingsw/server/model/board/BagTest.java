686c4f438675a25b7964cc065b64384ae89fd65a
RiccardoMilici <miliciriccardo@tiscali.it>
2022-06-05 18:27:13 +0200

    Added Cloud and Bag tests.

---
+++ src/test/java/it/polimi/ingsw/server/model/board/BagTest.java
@@ -0,0 +1,138 @@
+package it.polimi.ingsw.server.model.board;
+
+import it.polimi.ingsw.utilities.HouseColor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test of class Bag.
+ *
+ * @author Riccardo Milici
+ */
+public class BagTest {
+
+    private Bag bag;
+
+    /**
+     * Generates a new GameBoard.
+     */
+    @BeforeEach
+    void setUp() {
+        this.bag = new Bag();
+    }
+
+    /**
+     * Allows the garbage collector to delete the created Assistant.
+     */
+    @AfterEach
+    void TearDown() {
+        this.bag = null;
+    }
+
+    /**
+     * Tests if the pop is performed correctly.
+     */
+    @Test
+    void pop() {
+        assertNotNull(bag.pop());
+    }
+
+    /**
+     * Tests if the correct status is returned.
+     */
+    @Test
+    void getStatus() {
+        Map<HouseColor, Integer> sampleContainedStudents = new EnumMap<>(HouseColor.class);
+        for(HouseColor color : HouseColor.values()) sampleContainedStudents.put(color, 24);
+
+        assertEquals(sampleContainedStudents, bag.getStatus());
+    }
+
+    /**
+     * Tests if the push is performed correctly.
+     */
+    @Test
+    void push() {
+        Map<HouseColor, Integer> sampleContainedStudents = new EnumMap<>(HouseColor.class);
+        for(HouseColor color : HouseColor.values()) sampleContainedStudents.put(color, 24);
+        sampleContainedStudents.replace(HouseColor.BLUE, 25);
+
+        bag.push(HouseColor.BLUE);
+        assertEquals(sampleContainedStudents, bag.getStatus());
+    }
+
+    /**
+     * Tests if the board setup is performed correctly.
+     */
+    @Test
+    void boardSetup() {
+        List<HouseColor> setUp = bag.boardSetUp();
+
+        assertTrue(setUp.size() == 10 && setUp.containsAll(Arrays.stream(HouseColor.values()).toList()));
+    }
+
+    /**
+     * Tests if a not empty bag is correctly identified.
+     */
+    @Test
+    void isNotEmpty() {
+        assertFalse(bag.isEmpty());
+    }
+
+    /**
+     * Tests if an empty bag is correctly identified.
+     */
+    @Test
+    void isEmpty() {
+        for(int i = 0; i<120; i++) bag.pop();
+
+        assertTrue(bag.isEmpty());
+    }
+
+    /**
+     * Tests if a restore is correctly performed.
+     */
+    @Test
+    void bagRestore() {
+        Map<HouseColor, Integer> sampleContainedStudents = new EnumMap<>(HouseColor.class);
+        sampleContainedStudents.put(HouseColor.RED, 20);
+        sampleContainedStudents.put(HouseColor.BLUE, 0);
+        sampleContainedStudents.put(HouseColor.YELLOW, 15);
+        sampleContainedStudents.put(HouseColor.GREEN, 22);
+        sampleContainedStudents.put(HouseColor.FUCHSIA, 4);
+
+        this.bag = new Bag(sampleContainedStudents);
+        sampleContainedStudents.remove(HouseColor.BLUE);
+
+        assertEquals(sampleContainedStudents, bag.getStatus());
+    }
+
+    /**
+     * Tests if two bags with the same content are correctly identified.
+     */
+    @Test
+    void bagsEquals() {
+        Bag sampleBag = new Bag();
+
+        assertEquals(sampleBag.getStatus(), bag.getStatus());
+        assertNotEquals(sampleBag, bag);
+    }
+
+    /**
+     * Tests if two bags with the same content have different hash codes due to the different students order into the stack.
+     */
+    @Test
+    void bagsHash() {
+        Bag sampleBag = new Bag();
+
+        assertNotEquals(sampleBag.hashCode(), bag.hashCode());
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/server/model/board/CloudTest.java
@@ -0,0 +1,138 @@
+package it.polimi.ingsw.server.model.board;
+
+import it.polimi.ingsw.utilities.HouseColor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.EnumMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test of class Cloud.
+ *
+ * @author Riccardo Milici
+ */
+public class CloudTest {
+
+    private Cloud cloud;
+
+    /**
+     * Generates a new GameBoard.
+     */
+    @BeforeEach
+    void setup() {
+        this.cloud = new Cloud(1);
+    }
+
+    /**
+     * Allows the garbage collector to delete the created Assistant.
+     */
+    @AfterEach
+    void tearDown(){
+        this.cloud = null;
+    }
+
+    /**
+     * Tests if the correct id is returned.
+     */
+    @Test
+    void getId() {
+        assertEquals(1, cloud.getId());
+    }
+
+    /**
+     * Tests if the correct students are returned.
+     */
+    @Test
+    void getStudents() {
+        Map<HouseColor, Integer> sampleStudents = new EnumMap<>(HouseColor.class);
+        for(HouseColor color : HouseColor.values()) sampleStudents.put(color, 0);
+
+        assertEquals(sampleStudents, cloud.getStudents());
+    }
+
+    /**
+     * Tests if an empty cloud is correctly identified.
+     */
+    @Test
+    void isNotFull() {
+        assertFalse(cloud.isFull());
+    }
+
+    /**
+     * Tests if a full cloud is correctly identified.
+     */
+    @Test
+    void isFull() {
+        Map<HouseColor, Integer> sampleStudents = new EnumMap<>(HouseColor.class);
+        for(HouseColor color : HouseColor.values()) sampleStudents.put(color, 1);
+
+        cloud.refill(sampleStudents);
+        assertTrue(cloud.isFull());
+    }
+
+    /**
+     * Tests if a cloud refill is correctly performed.
+     */
+    @Test
+    void refill() {
+        Map<HouseColor, Integer> sampleStudents = new EnumMap<>(HouseColor.class);
+        for(HouseColor color : HouseColor.values()) sampleStudents.put(color, 1);
+
+        cloud.refill(sampleStudents);
+        assertEquals(sampleStudents, cloud.getStudents());
+    }
+
+    /**
+     * Tests if a cloud flush is correctly performed.
+     */
+    @Test
+    void flush() {
+        Map<HouseColor, Integer> sampleStudents = new EnumMap<>(HouseColor.class);
+        for(HouseColor color : HouseColor.values()) sampleStudents.put(color, 1);
+
+        cloud.refill(sampleStudents);
+        cloud.flush();
+        assertFalse(cloud.isFull());
+    }
+
+    /**
+     * Tets if a cloud restore is correctly performed.
+     */
+    @Test
+    void cloudRestore() {
+        Map<HouseColor, Integer> sampleStudents = new EnumMap<>(HouseColor.class);
+        for(HouseColor color : HouseColor.values()) sampleStudents.put(color, 1);
+        this.cloud = new Cloud(1, sampleStudents);
+
+        assertEquals(sampleStudents, cloud.getStudents());
+    }
+
+    /**
+     * Tests if two identical clouds are recognized.
+     */
+    @Test
+    void cloudsEquals() {
+        Map<HouseColor, Integer> sampleStudents = new EnumMap<>(HouseColor.class);
+        for(HouseColor color : HouseColor.values()) sampleStudents.put(color, 1);
+        Cloud sampleCloud = new Cloud(1, sampleStudents);
+        cloud.refill(sampleStudents);
+
+        assertEquals(cloud, sampleCloud);
+    }
+
+    /**
+     * Tests if two identical clouds have the same hash code.
+     */
+    @Test
+    void cloudsHash() {
+        Map<HouseColor, Integer> sampleStudents = new EnumMap<>(HouseColor.class);
+        for(HouseColor color : HouseColor.values()) sampleStudents.put(color, 1);
+        Cloud sampleCloud = new Cloud(1, sampleStudents);
+        cloud.refill(sampleStudents);
+
+        assertEquals(cloud.hashCode(), sampleCloud.hashCode());
+    }
+}
